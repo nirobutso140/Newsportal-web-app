1d7b9c554d1fb571929af89fdd48aaf8d30ce1e5
nirobutso140 <nirobutso140@gmail.com>
2023-11-26 00:24:16 +0600

    create multi tag input field and select multiple tag

--- src/pages/ArticleAdding/Artical.jsx
+++ src/pages/ArticleAdding/Artical.jsx
@@ -1,29 +1,40 @@
 import Title from '../../components/Container/Title';
 import './Article.css'
+import Select from 'react-select';
+
 
 const Artical = () => {
+
+    const options = [
+        { value: 'Technology', label: 'Technology' },
+        { value: 'Business', label: 'Business' },
+        { value: 'Science', label: 'Science' },
+        { value: 'Health', label: 'Health' },
+        { value: 'Sports', label: 'Sports' },
+      ];
+
     return (
         <>
-        
+
             <div className="py-24 form">
-            <Title>Add Article</Title>
+                <Title>Add Article</Title>
                 <form>
                     {/* form name and quantity row */}
                     <div className="mb-8">
                         <div className="form-control w-full">
                             <label className="label">
-                               <span className="label-text text-green-600">Your Name</span>
+                                <span className="label-text text-green-600">Title</span>
                             </label>
                             <label className="input-group">
-                                <input type="text" name="service" placeholder="Service Name" className="input input-bordered w-full" />
+                                <input type="text" name="service" placeholder="Title" className="input input-bordered w-full" />
                             </label>
                         </div>
                         <div className="form-control w-full">
                             <label className="label">
-                                <span className="label-text text-green-600">Your Name</span>
+                                <span className="label-text text-green-600">Publisher Name</span>
                             </label>
                             <label className="input-group w-full">
-                                <input type="text" name="name" placeholder="Your Name" className="input input-bordered w-full"  readOnly />
+                                <input type="text" name="name" placeholder="Publisher Name" className="input input-bordered w-full" readOnly />
                             </label>
                         </div>
                     </div>
@@ -31,10 +42,10 @@ const Artical = () => {
                     <div className="mb-8">
                         <div className="form-control w-full">
                             <label className="label">
-                                <span className="label-text text-green-600">Your Email</span>
+                                <span className="label-text text-green-600">Article Description</span>
                             </label>
                             <label className="input-group">
-                                <input type="text" name="email" placeholder="Your Email" className="input input-bordered w-full" readOnly />
+                                <input type="text" name="email" placeholder="Article Description" className="input input-bordered w-full" readOnly />
                             </label>
                         </div>
                         <div className="form-control w-full">
@@ -50,16 +61,16 @@ const Artical = () => {
                     <div className="mb-8">
                         <div className="form-control w-full">
                             <label className="label">
-                                <span className="label-text text-green-600">Service Area</span>
+                                <span className="label-text text-green-600">Articles Tag</span>
                             </label>
-                            <label className="input-group">
-                                <input type="text" name="area" placeholder="Service Area" className="input input-bordered w-full" />
+                            <label className="input-group w-full">
+                               <Select options={options} isMulti/>
                             </label>
                         </div>
                     </div>
                     {/* form Photo url row */}
 
-                    <input type="submit" value="Add Article" className="btn btn-block btn-secondary bg-fuchsia-600 w-full"/>
+                    <input type="submit" value="Add Article" className="btn btn-block btn-secondary bg-fuchsia-600 w-full" />
 
                 </form>
             </div>
